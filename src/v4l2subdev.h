7266b1b5433b5644a06f05edf61c36864ab11683
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2011-10-05 21:41:22 +0200

    Switch to the LGPLv2.1+ license
    Mail sent to all contributors on 2011-09-30:
    
    Hi everybody,
    
    Sakari made me realize today that the media-ctl project is still licensed
    under the GPL v2+ license, while I would have sworn that we had switched to
    LGPL a long time ago.
    
    I would like to relicense the media-ctl project under the LGPL v2.1+, to allow
    LGPL-compatible applications and libraries (such as libv4l for instance, or
    its plugins) to link to libmediactl and libv4l2subdev.
    
    As you all have submitted patches that are now included in the media-ctl
    project, I'm requesting your permission to proceed with the license change.
    
    --------------------------------------------------------------------------------
    From: Todor Tomov <ttomov@mm-sol.com>
      To: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Date: 2011-09-30 12:06:17
    
    Hi Laurent,
    
    No problems from my side.
    
    --
    Best regards,
    Todor Tomov
    
    --------------------------------------------------------------------------------
    From: "Aguirre, Sergio" <saaguirre@ti.com>
      To: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Date: 2011-09-30 15:41:45
    
    Hi Laurent,
    
    No problem for me. You can go ahead, if everyone else agrees of course.
    
    Regards,
    Sergio
    
    --------------------------------------------------------------------------------
    From: Sakari Ailus <sakari.ailus@iki.fi>
      To: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Date: 2011-09-30 16:17:18
    
    I'm in favour of the change, actually I think it's next to mandatory for the
    library to be useful in a way it was intended.
    
    Cheers,
    
    --
    Sakari Ailus
    
    --------------------------------------------------------------------------------
    From: Michael Jones <michael.jones@matrix-vision.de>
      To: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Date: 2011-10-04 08:33:17
    
    Changing the license is fine with me.
    
    -Michael
    
    --------------------------------------------------------------------------------
    From: Andy Shevchenko <andriy.shevchenko@linux.intel.com> (Intel Finland Oy)
      To: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Date: 2011-10-10 14:26:00
    
    I have no objections.
    But I didn't remember libudev exact license... Ah, okay, it's licensed
    under LGPLv2.1+.
    
    --
    Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Intel Finland Oy
    
    --------------------------------------------------------------------------------
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- src/v4l2subdev.h
+++ src/v4l2subdev.h
@@ -1,20 +1,22 @@
 /*
  * V4L2 subdev interface library
  *
- * Copyright (C) 2010 Ideas on board SPRL <laurent.pinchart@ideasonboard.com>
+ * Copyright (C) 2010-2011 Ideas on board SPRL
+ *
+ * Contact: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef __SUBDEV_H__
